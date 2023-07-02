5d321f05857c5a2a7ce57b2d9803f0b9905a8077
koyo code <koyo20010815@gmail.com>
2023-07-02 18:34:14 +0900

    なんかバグってる

--- src/app/components/Items.tsx
+++ src/app/components/Items.tsx
@@ -59,7 +59,7 @@ export default function Items(contents: Blog) {
 
   return (
     <>
-      <input className="ml-auto block rounded mb-10 py-1 md:py-2 px-2 md:px-3 text-base font-medium theme-item" placeholder="Search" type="text" value={inputValue} onChange={handleChange} />
+      <input className="ml-auto block w-48 md:w-60 rounded mb-10 py-1 md:py-2 px-2 md:px-3 text-base font-medium theme-item" placeholder="Search" type="text" value={inputValue} onChange={handleChange} />
       <PageWrapper>
         <motion.ul variants={variants} initial="hidden" animate="show" className="w-full grid md:grid-cols-3 grid-cols-2 gap-3 md:gap-5">
           {memberList.map((member, index) => {
@@ -69,7 +69,7 @@ export default function Items(contents: Blog) {
                   <div className="">
                     <h3 className="label font-bold flex justify-center items-center text-4xl md:text-5xl w-full h-32 md:h-44 bg-transparent">{member.genre[0]}</h3>
                     <div className="p-3 md:p-4">
-                      <p className="text-right text-xs">{new Date(member.updatedAt).toLocaleDateString()}</p>
+                      {/* <p className="text-right text-xs">{new Date(member.updatedAt).toLocaleDateString()}</p> */}
                       <h3 className="font-bold text-sm md:text-lg mb-2">{member.title}</h3>
                       <p className="text-xs md:text-sm exp-line">{member.content}</p>
                     </div>
