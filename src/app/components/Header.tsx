5d5bcc5c66665c816fd11b5f61b032f782491cee
koyo code <koyo20010815@gmail.com>
2023-07-09 21:04:02 +0900

    バグ原因わかるまで戻す

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="flex items-center justify-between p-4 md:p-5 ">
       <div>
         <p className="text-2xl md:text-3xl font-bold">
-          <Link href="/">LIB</Link>
+          <Link href="/">LIBRARY</Link>
         </p>
       </div>
       <ThemeButton />
